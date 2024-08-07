b113dec59d2751ae1b627bcc1d481aba085faf13
BerezhnovA <anton.nvrsk@gmail.com>
2024-08-07 22:17:13 +0300

    Создал user & Category

--- src/main/java/com/example/diplom/entity/Category.java
+++ src/main/java/com/example/diplom/entity/Category.java
@@ -1,4 +1,27 @@
 package com.example.diplom.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long category_id;
+    private String category_name;
+
+    public Category(Long category_id, String category_name) {
+        this.category_id = category_id;
+        this.category_name = category_name;
+    }
 }
+
