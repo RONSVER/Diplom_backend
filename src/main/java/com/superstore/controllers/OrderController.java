8135956544fb9a933f939203f547877cb42782f8
gribodemon <gribodemong@gmail.com>
2024-08-20 13:13:46 +0300

    refactoring

--- src/main/java/com/superstore/controllers/OrderController.java
+++ src/main/java/com/superstore/controllers/OrderController.java
@@ -2,7 +2,6 @@ package com.superstore.controllers;
 
 import com.superstore.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
