aa87ab068784c861fff01ae0bc8739626e5a0a61
Anton.nvrsk <anton.nvrsk@gmial.com>
2024-09-15 19:48:58 +0300

    Добавил CartControllerSwagger и FavoriteControllerSwagger и ReportControllerSwagger  описания

--- src/main/java/com/superstore/controllers/CartController.java
+++ src/main/java/com/superstore/controllers/CartController.java
@@ -1,11 +1,9 @@
 package com.superstore.controllers;
 
+import com.superstore.controllers.swagger.CartControllerSwagger;
 import com.superstore.dto.CartDto;
 import com.superstore.services.CartService;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,56 +16,44 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/cart")
 @AllArgsConstructor
-public class CartController {
+public class CartController implements CartControllerSwagger {
 
 
 
     private CartService service;
 
     @GetMapping
+    @Override
     public ResponseEntity<CartDto> getCart() {
         return ResponseEntity.ok(service.getCart());
     }
 
     @PostMapping("/add")
+    @Override
     public ResponseEntity<CartDto> addProductToCart(@RequestBody AddProductToCartRequest addProductToCartRequest) {
         CartDto cartDto = service.addProductToCart(addProductToCartRequest.getProductId(), addProductToCartRequest.getQuantity());
         return ResponseEntity.ok(cartDto);
     }
 
     @DeleteMapping("/remove/{productId}")
+    @Override
     public ResponseEntity<Void> removeProductFromCart(@PathVariable Long productId) {
         service.removeProductFromCart(productId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/clear")
+    @Override
     public ResponseEntity<Void> clearCart() {
         service.clearCart();
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/update-quantity")
+    @Override
     public ResponseEntity<Void> updateCartItemQuantity(@RequestBody UpdateCartItemQuantityRequest request) {
         service.updateCartItemQuantity(request.getCartItemId(), request.getQuantity());
         return ResponseEntity.ok().build();
     }
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class AddProductToCartRequest {
-        private Long productId;
-        private Integer quantity;
-    }
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    static public class UpdateCartItemQuantityRequest {
-        private Long cartItemId;
-        private Integer quantity;
-    }
 }
---
+++ src/main/java/com/superstore/controllers/swagger/FavoriteControllerSwagger.java
@@ -0,0 +1,54 @@
+package com.superstore.controllers.swagger;
+
+import com.superstore.dto.FavoriteDto;
+import com.superstore.dto.ProductDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+public interface FavoriteControllerSwagger {
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('Client') or hasAuthority('Administrator')")
+    @Operation(
+            summary = "Получение списка избранных товаров пользователя",
+            description = "Показывает списка избранных товаров пользователя",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ок"),
+                    @ApiResponse(responseCode = "401", description = "Не авторизован")
+            }
+    )
+    ResponseEntity<List<ProductDto>> getUserFavorites();
+
+    @PostMapping("/{productId}")
+    @PreAuthorize("hasAuthority('Client') or hasAuthority('Administrator')")
+
+    @Operation(
+            summary = "Добавление товара в избранное",
+            description = "Добавляет товар в избранное",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ок"),
+                    @ApiResponse(responseCode = "401", description = "Не авторизован")
+            }
+    )
+    ResponseEntity<FavoriteDto> addFavorite(@PathVariable Long productId);
+
+    @DeleteMapping("/{productId}")
+    @PreAuthorize("hasAuthority('Client') or hasAuthority('Administrator')")
+    @Operation(
+            summary = "Удаление товара из избранного",
+            description = "Удаляет товар из избранного",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ок"),
+                    @ApiResponse(responseCode = "401", description = "Не авторизован")
+            }
+    )
+    ResponseEntity<Void> removeFavorite(@PathVariable Long productId);
+}
